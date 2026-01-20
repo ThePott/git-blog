fccb29694fe05dc9053a296c7972ab50756a661c
Tynahan <nusilite@gmail.com>
2026-01-20 20:51:08 +0900

    coding-test: recursion revision failed

---
+++ src/coding-test/my-weapons/combination/3_revised.js
@@ -0,0 +1,47 @@
+// TODO: combine letters of two, in alphabetic order
+
+/**
+ @param {Object} props
+ @param {string[]} props.targetArray
+ @param {number} props.count
+
+ @returns {string[][]}  array of combination
+*/
+const combination = ({ targetArray, count }) => {
+    const result = []
+
+    // NOTE: result, current만 mutate 함
+    const recursiveFn = ({ index, current }) => {
+        const element = targetArray[index]
+        console.log({ index, current, element })
+        if (!element) throw new Error("SHOULD HAVE STOPPED BEFORE FALL HERE")
+
+        const newCurrent = [...current, targetArray[index]]
+        if (newCurrent.length > count) throw new Error("SHOULD NOT FALL HERE")
+
+        // NOTE: 남은 걸 다 넣어도 부족하면 루프 끝냄
+        if (newCurrent.length + targetArray.length - index - 1 < count) return
+
+        // NOTE: 개수 딱 맞으면 합치고 해당 루프 끝냄
+        if (newCurrent.length === count) {
+            result.push(newCurrent)
+            return
+        }
+
+        // NOTE: 아직 남았으면
+        // NOTE: 재료에 넣고 마저 돌림
+        current.push(element)
+
+        for (let i = index + 1; i < targetArray.length; i++) {
+            recursiveFn({ index: i, current: newCurrent })
+        }
+    }
+
+    // NOTE: 아직 좀 덜 됐다...
+    targetArray.forEach((_, index) => recursiveFn({ index, current: [] }))
+
+    return result
+}
+
+const result = combination({ targetArray: ["a", "b", "c", "d"], count: 3 })
+console.log({ result })
